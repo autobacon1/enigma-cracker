45e73ba3384b139004b9accfa968409b539df5fe
autobacon1 <62883825+autobacon1@users.noreply.github.com>
2022-02-09 23:27:10 +0000

    swapletter function added

--- main.go
+++ main.go
@@ -8,10 +8,11 @@ import (
 
 func main() {
 
-	plugboardPairs := make([]string, 20)
+	plugboardPairs := generate()
 	fmt.Println(plugboardPairs)
-	plugboardPairs = generate()
-	fmt.Println(plugboardPairs)
+
+	returnLetter := swapLetter(plugboardPairs, plugboardPairs[0])
+	fmt.Println(returnLetter)
 }
 
 func generate() []string {
@@ -25,4 +26,30 @@ func generate() []string {
 		letters = append(letters[:index], letters[index+1:]...)
 	}
 	return pairs
+}
+
+func generate2(letters []string) []string {
+	//letters := []string{"A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"}
+	pairs := make([]string, 20)
+	rand.Seed(time.Now().UnixNano())
+
+	for i := 0; i < 20; i++ {
+		index := rand.Intn(len(letters))
+		pairs[i] = letters[index]
+		letters = append(letters[:index], letters[index+1:]...)
+	}
+	return pairs
+}
+
+func swapLetter(plugboard []string, character string) string {
+	for i := 0; i < len(plugboard); i++ { //find index of input character
+		if plugboard[i] == character { //find index of character in plugboard
+			if (i % 2) == 0 { //find whether index is odd or even
+				return plugboard[i+1] //if even, return the next character
+			} else {
+				return plugboard[i-1] //if odd, return the previous character
+			}
+		}
+	}
+	return character //not used on plugboard
 }
--- main_test.go
+++ main_test.go
@@ -36,3 +36,19 @@ func BenchmarkGeneratePairs(b *testing.B) {
 		generate()
 	}
 }
+
+func BenchmarkGeneratePairs2(b *testing.B) {
+	letters := []string{"A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"}
+	b.ResetTimer()
+	for i := 0; i < b.N; i++ {
+		generate2(letters)
+	}
+}
+
+func BenchmarkSwapLetter(b *testing.B) {
+	pairs := generate()
+	b.ResetTimer()
+	for i := 0; i < b.N; i++ {
+		swapLetter(pairs, pairs[0])
+	}
+}
